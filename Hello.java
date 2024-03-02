e1c88f545fe34a3a98d8dc7ebfdf9935025e0a01
umeshlakhe <umesh@qable.io>
2024-03-02 12:58:40 +0530

    this is my first modify commit

--- Hello.java
+++ Hello.java
@@ -1,4 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
+
+        int a =20;
+        int b=40;
+        int c = a+b;
+        System.out.println(c);
         System.out.println("Hello");
+
+
     }}
